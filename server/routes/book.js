59d5534cdde231198349fa9cca72abb3be4bcbb6
VanLamCS <levanlam7602@gmail.com>
2023-03-20 14:41:19 +0700

    modified: routes

--- server/routes/book.js
+++ server/routes/book.js
@@ -1,15 +1,15 @@
 import express from "express";
 import * as bookController from "../controllers/BookController.js";
+import * as authMiddleware from "../middlewares/authMiddleware.js";
 
 const bookRoute = express.Router();
 
 bookRoute.get("/get", bookController.getBooksByCategory);
 bookRoute.get("/get/:id", bookController.getBookById);
 
-bookRoute.post("/create", bookController.addBook);
+bookRoute.post("/create", authMiddleware.verifyToken, bookController.addBook);
 bookRoute.delete("/delete", bookController.deleteBook);
 bookRoute.patch("/update", bookController.updateBook);
-bookRoute.patch('/update-star', bookController.updateStar);
-
+bookRoute.patch("/update-star", bookController.updateStar);
 
 export default bookRoute;
